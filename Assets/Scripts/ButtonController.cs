eddd8b7dc3103c74bd4368b532bf2252c90cbe09
Peter <34187808+RetroPete@users.noreply.github.com>
2020-08-24 20:05:44 +0100

    Changes to game files

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -7,15 +7,76 @@ using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
+	public GameObject player;
+	
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+	
+	{
+         player = GameObject.FindWithTag ("Player");
+     }
+	
+    //{
+    //    player.GetComponent<PlayerController>();
+    //}
 
     // Update is called once per frame
     void Update()
     {
         
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(new Vector3(0, .5f, 0) * player.GetComponent<PlayerController>().autoMove * Time.deltaTime);
+		}
+		
+		if (Input.GetKey(KeyCode.S))
+		{
+			player.GetComponent<PlayerController>().transform.Translate(new Vector3(0, -.5f, 0) * player.GetComponent<PlayerController>().autoMove * Time.deltaTime);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.D))
+        {
+			player.GetComponent<PlayerController>().horizontal = 1;
+        }
+		
+		if (Input.GetKeyUp(KeyCode.D))
+		{
+			player.GetComponent<PlayerController>().horizontal = 0;
+        }
+		
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			//player.GetComponent<PlayerController>().StartCoroutine(RollUp());
+		}
+		
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			//player.GetComponent<PlayerController>().StartCoroutine(RollDown());
+		}
+    }
+	
+	public void Idle()
+	{
+		transform.Translate(new Vector3(0, 0, 0) * player.GetComponent<PlayerController>().autoMove * Time.deltaTime);
+	}
+	
+	public void MoveUp()
+	{
+		transform.Translate(Vector3.up * player.GetComponent<PlayerController>().autoMove * Time.deltaTime);
+	}
+		
+	public void MoveDown()
+	{
+		transform.Translate(new Vector3(0, -.5f, 0) * player.GetComponent<PlayerController>().autoMove * Time.deltaTime);
+	}
+		
+	public void BoostOn()
+    {
+		player.GetComponent<PlayerController>().horizontal = 1;
+    }
+		
+	public void BoostOff()
+	{
+		player.GetComponent<PlayerController>().horizontal = 0;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,8 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-	private float horizontal = 0;
-    private float vertical = 0;
+	public float horizontal;
+    public float vertical;
 	
 	public float boostSpeed;
 	public float autoMove;
@@ -38,36 +38,6 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
     void Update()
     {
-		if (Input.GetKey(KeyCode.W))
-		{
-			transform.Translate(new Vector3(0, .5f, 0) * autoMove * Time.deltaTime);
-		}
-		
-		if (Input.GetKey(KeyCode.S))
-		{
-			transform.Translate(new Vector3(0, -.5f, 0) * autoMove * Time.deltaTime);
-		}
-		
-		if (Input.GetKeyDown(KeyCode.D))
-        {
-			horizontal = 1;
-        }
-		
-		if (Input.GetKeyUp(KeyCode.D))
-		{
-			horizontal = 0;
-        }
-		
-		if (Input.GetKeyDown(KeyCode.Q))
-		{
-			StartCoroutine(RollUp());
-		}
-		
-		if (Input.GetKeyDown(KeyCode.E))
-		{
-			StartCoroutine(RollDown());
-		}
-		
 		
 		Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp01(pos.x);
