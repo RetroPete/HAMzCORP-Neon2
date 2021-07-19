2779dd0655ba8d504b48adbe10f3b2b521682c63
Peter <34187808+RetroPete@users.noreply.github.com>
2021-07-19 19:37:32 +0100

    Changes to game files

---
+++ Assets/Scripts/PlayerRollController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerRollController : MonoBehaviour
+{
+	public float rollSpeed;
+	
+	public GameObject player;
+	
+	private Animator anim;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.GetComponent<PlayerEnergy>();
+		
+		anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			StartCoroutine(RollUp());
+		}
+		
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			StartCoroutine(RollDown());
+		}
+    }
+	
+	IEnumerator RollUp()
+	{
+		yield return new WaitForSeconds(.2f);
+		
+		transform.Translate(new Vector3(0, 1, 0) * rollSpeed);
+		
+		player.GetComponent<PlayerEnergy>().TakeEnergy(100);
+		
+		anim.Play("player_roll_up"); 
+	}
+	
+	IEnumerator RollDown()
+	{
+		yield return new WaitForSeconds(.2f);
+		
+		transform.Translate(new Vector3(0, -1, 0) * rollSpeed);
+		
+		player.GetComponent<PlayerEnergy>().TakeEnergy(100);
+		
+		anim.Play("player_roll_down");
+	}
+}
